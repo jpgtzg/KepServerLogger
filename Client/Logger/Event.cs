5d083a69109c1a34491389ff0a6aa60506cfaead
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2026-03-04 11:27:27 -0600

    feat: added event class mimicking kepserver's event log

---
+++ Client/Logger/Event.cs
@@ -0,0 +1,25 @@
+using System.Text.Json.Serialization;
+namespace Events
+{
+
+    public class Event
+    {
+        [JsonPropertyName("timestamp")]
+        public string Timestamp { get; }
+        [JsonPropertyName("event")]
+        public string Name { get; }
+        [JsonPropertyName("source")]
+        public string Source { get;  }
+
+        [JsonPropertyName("message")]
+        public string Message { get; }
+        
+        public Event(string timestamp, string name, string source, string message)
+        {
+            Timestamp = timestamp;
+            Name = name;
+            Source = source;
+            Message = message;
+        }
+    }
+}
