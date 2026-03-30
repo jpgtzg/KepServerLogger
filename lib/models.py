e0706aafc08c15bf5eac70323581f452df1d9f42
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2026-03-30 11:00:46 -0600

    feat: add data models for metrics including CPU, RAM, Network usage, and service information

---
+++ lib/models.py
@@ -0,0 +1,50 @@
+"""
+This module contains the models for the data that is ingested into the database.
+"""
+
+from __future__ import annotations
+from pydantic import BaseModel
+
+
+class Tag(BaseModel):
+    tag: str
+    value: str
+    status_code: str
+    source_timestamp: str
+    server_timestamp: str
+
+
+class CPUUsage(BaseModel):
+    timestamp: str
+    usage: float
+
+
+class RAMUsage(BaseModel):
+    timestamp: str
+    total_kb: int
+    free_kb: int
+
+
+class NetworkUsage(BaseModel):
+    timestamp: str
+    interface: str
+    operational_status: str
+    network_interface_type: str
+    kb_bytes_sent: float
+    kb_bytes_received: float
+
+
+class ServiceInfo(BaseModel):
+    name: str
+    status: str
+    service_type: str
+    machine_name: str
+    process_ids: list[int]
+
+
+class KepEvent(BaseModel):
+    timestamp: str
+    name: str
+    source: str
+    message: str
+    hash: str
