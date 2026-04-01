865280f1736f5acf8e0cc2d0b500a63e26f3168d
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2026-03-31 19:39:51 -0600

    feat: add service and event metrics to metrics module, expanding available metrics for comprehensive monitoring

--- extractors/src/metrics/__init__.py
+++ extractors/src/metrics/__init__.py
@@ -1,9 +1,13 @@
 from metrics.cpu import get_total_cpu_usage
 from metrics.network import get_network_interfaces
 from metrics.ram import get_memory_info
+from metrics.services import get_service_info
+from metrics.events import get_kepserver_events
 
 __all__ = [
     "get_total_cpu_usage",
     "get_memory_info",
     "get_network_interfaces",
+    "get_service_info",
+    "get_kepserver_events",
 ]
