dbdb1e32e6f81b53318fb11c2225a9561f1583af
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2026-03-31 19:40:38 -0600

    feat: implement subscription for Kep events in OPCUA client, enabling retrieval of event data for monitoring

---
+++ ingestors/subscribers/opcua.py
@@ -0,0 +1,12 @@
+from asyncua import ua
+from lib.config import settings
+from lib.opcua_client import OPCUAClient
+from lib.models import KepEvent
+import json
+
+async def subscribe_kep_events(client: OPCUAClient) -> None:
+    node = client.get_node(f"{settings.metrics_config.events.prefix}.batch")
+    raw: str = await node.read_value()
+    if not raw:
+        return []
+    return [KepEvent(**e) for e in json.loads(raw)]
