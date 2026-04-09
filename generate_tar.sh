8cc0323c9f49dff95d558107eb90e5eef69116d3
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2026-04-09 09:44:23 -0600

    feat: add script to build and export Docker image as tar.gz

---
+++ generate_tar.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+DOCKERFILE_PATH ="$1"
+IMAGE_NAME="$2"
+
+if [ -z "$DOCKERFILE_PATH" ] || [ -z "$IMAGE_NAME" ]; then
+    echo "Usage: $0 <Dockerfile_path> <image_name>"
+    exit 1
+fi
+
+docker build -f $DOCKERFILE_PATH -t $IMAGE_NAME . 2>&1 | tail -5
+docker save $IMAGE_NAME | gzip > $IMAGE_NAME.tar.gz && echo "done"
