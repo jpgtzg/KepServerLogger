f483615d115ae1779a8b002648e4f018b4d605a8
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2026-04-13 10:56:28 -0600

    feat: add certgen build and --timescale flag
    build.ps1: add [switch] $certgen to run pyinstaller on utils\certgen
    generate_tar.sh: add --timescale to pull/save
    timescale/timescaledb-ha:pg16
    and make Dockerfile/image args optional; update usage message
    
    Signed-off-by: jpgtzg <58833781+jpgtzg@users.noreply.github.com>

--- build.ps1
+++ build.ps1
@@ -1,2 +1,13 @@
-Set-Location "$PSScriptRoot\extractors"
+param (
+    [switch]$certgen
+)
+
+if ($certgen) {
+    Push-Location .\utils\certgen
+    uv run pyinstaller certgen.spec --noconfirm
+    Pop-Location
+}
+
+Push-Location .\extractors
 uv run pyinstaller extractor.spec --noconfirm
+Pop-Location
--- generate_tar.sh
+++ generate_tar.sh
@@ -1,13 +1,29 @@
 #!/bin/sh
 set -e
 
+TIMESCALE_IMAGE="timescale/timescaledb-ha:pg16"
+BUILD_TIMESCALE=0
+
+# Parse optional --timescale flag
+if [ "$1" = "--timescale" ]; then
+    BUILD_TIMESCALE=1
+    shift
+fi
+
 DOCKERFILE_PATH="$1"
 IMAGE_NAME="$2"
 
-if [ -z "$DOCKERFILE_PATH" ] || [ -z "$IMAGE_NAME" ]; then
-    echo "Usage: $0 <Dockerfile_path> <image_name>"
+if [ "$BUILD_TIMESCALE" = "0" ] && { [ -z "$DOCKERFILE_PATH" ] || [ -z "$IMAGE_NAME" ]; }; then
+    echo "Usage: $0 [--timescale] [<Dockerfile_path> <image_name>]"
     exit 1
 fi
 
-docker build -f $DOCKERFILE_PATH -t $IMAGE_NAME . 2>&1 | tail -5
-docker save $IMAGE_NAME | gzip > $IMAGE_NAME.tar.gz && echo "done at "$(date +"%H:%M:%S")
+if [ "$BUILD_TIMESCALE" = "1" ]; then
+    docker pull "$TIMESCALE_IMAGE"
+    docker save "$TIMESCALE_IMAGE" | gzip > timescaledb.tar.gz && echo "timescaledb done at $(date +"%H:%M:%S")"
+fi
+
+if [ -n "$DOCKERFILE_PATH" ] && [ -n "$IMAGE_NAME" ]; then
+    docker build -f "$DOCKERFILE_PATH" -t "$IMAGE_NAME" . 2>&1 | tail -5
+    docker save "$IMAGE_NAME" | gzip > "$IMAGE_NAME.tar.gz" && echo "done at $(date +"%H:%M:%S")"
+fi
